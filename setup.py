c5fc106bceba62f0eb319af1caa1347bc7c15b52
Sadaf <sadafsoomro53@gmail.com>
2025-11-08 02:45:07 -0800

    requirements addded

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages, setup 
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="sadaf soomro",
+    author_email="sadafsoomro53@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
